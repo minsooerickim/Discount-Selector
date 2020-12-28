79ce539b5014cdad50d2d37c541705f142b87761
Minsoo Kim <minsooerickim@gmail.com>
2020-12-27 20:03:13 -0800

    found discounted items and its original prices

--- content.js
+++ content.js
@@ -1,1 +1,28 @@
-console.log('test')
+$(function() {
+    //orginal prices of discounted items
+    var items = [];
+    i = 0;
+    var currentPrice = [];
+    j = 0;
+    
+    $('span.a-price.a-text-price span.a-offscreen').each(function(){
+        items[i++] = $(this).text();
+        currentPrice[j++] = $(this).parent().prev('span.a-price').find('span.a-offscreen').text();
+    });
+    console.log(items);
+
+    //discounted/current price
+    
+    //$('span.a-price.a-text-price span.a-offscreen').('span.a-price span.a-offscreen').each(function(){
+    //    currentPrice[j++] = $(this).text();
+    //});
+    //console.log(items);
+    //var currentPrice = $("span.a-price.a-text-price").closest(":has(.a-offscreen)").find('.a-offscreen').text();
+    console.log(currentPrice);
+
+    //for (i = 0; i < items.length; i++) {
+    //    console.log(items[i] + ": " + currentPrice[i])
+    //    i++;
+    //}
+});
+
