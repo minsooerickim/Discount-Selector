3e0356f578c034d57286aea42bd29b1dff27a843
Minsoo Kim <minsooerickim@gmail.com>
2020-12-28 15:51:18 -0800

    get rid of duplicates

--- content.js
+++ content.js
@@ -22,6 +22,73 @@ $(function() {
         dataAsin[k++] = $(this).parents('div[data-asin]').attr('data-asin');
     });
 
+    dataAsin.forEach((c, index) => {
+        console.log(`${c} - ${index} - ${dataAsin.indexOf(c)}`);
+    });
+    console.log(dataAsin);
+    /*
+    //getting rid of duplicate items
+    var indexOf = [];
+    q = 0;
+    dataAsin.forEach((c, index) => {
+        indexOf[q++] = (`${dataAsin.indexOf(c)}`);
+    });
+    console.log(indexOf)
+    for (var i = 0; i < items.length; i++) {
+        if (i != indexOf[i]) {
+            data.splice(i, 1);
+            items.splice(i, 1);
+            currentPrice.splice(i, 1);
+        }
+    }
+    console.log(dataAsin);
+*/
+    Array.prototype.getDuplicates = function () {
+        var duplicates = {};
+        for (var i = 0; i < this.length; i++) {
+            if(duplicates.hasOwnProperty(this[i])) {
+                duplicates[this[i]].push(i);
+            } else if (this.lastIndexOf(this[i]) !== i) {
+                duplicates[this[i]] = [i];
+            }
+        }
+    
+        return duplicates;
+    };
+    var dupes = [];
+    var dupesLength = [];
+    var dupeValues = [];
+    p = 0;
+    var duplicates = dataAsin.getDuplicates();
+    console.log(duplicates);
+    for(var prop in duplicates) {
+        dupes[p++] = (prop, duplicates[prop]);
+        dupesLength[p++] = duplicates[prop].length;
+        if (duplicates[prop].length  == 2) {
+            dupeValues[p] = duplicates[prop][duplicates[prop].length - 1]
+            dataAsin.splice((dupeValues[p]), 1);
+            p+=1;
+        }
+        else {
+            var length = duplicates[prop].length;
+            while (length != 1) {
+                dupeValues[p] = duplicates[prop][length- 1];
+                dataAsin.splice(dupeValues[p], 1)
+                p+=1;
+                length -= 1;
+            }
+        }
+    }
+    console.log(dataAsin);
+    console.log(dupes);
+    console.log(dupesLength);
+    console.log(dupeValues);
+    //for (var i = 0; i < dupes.length; i++) {
+    //    if (dupesLength[i] == 2) {
+    //        dataAsin.slice(duplicates[prop])
+    //    }
+    //}
+
     //getting price diff list
     for (var i = 0; i < items.length; i++) {
         priceDiff[i] = items[i] - currentPrice[i];
@@ -43,17 +110,43 @@ $(function() {
     });
     
     reverseSortedPriceDiff = sortedPriceDiff.reverse();
-
-    console.log(priceDiff);
-    console.log(reverseSortedPriceDiff);
+    /*
+    console.log(items);
+    console.log(currentPrice);
     console.log(dataAsin);
-
+    console.log(priceDiff);
+    console.log(intOrgPrice);
+    console.log(intDiscPrice);
+    console.log(sortedPriceDiff);
+    console.log(reverseSortedPriceDiff);
+    */
     var l = (document.getElementsByClassName('sg-col-4-of-12')[1]);
     var firstDataAsinVal = l.getAttribute('data-asin');
 
+    //moving all discounted items up
     for (var i = items.length - 1; i >= 0; i--) {
         $('div[data-asin="' + dataAsin[i] + '"]').insertBefore('div[data-asin="' + firstDataAsinVal + '"]');
     }
+    
+    //find correct index placements
+    var moveIndexPos = [];
+    z = 0;
+    for (var i = 0; i < reverseSortedPriceDiff.length; i++) {
+        moveIndexPos[z++] = reverseSortedPriceDiff.indexOf(priceDiff[i]);
+    }
+    console.log(moveIndexPos);
+    /*
+    //putting them according to moveIndexPos
+    for (var i = 0; i < items.length; i++) {
+        $('div[data-asin="' + dataAsin[i] + '"]').insertBefore('div[data-asin="' + (moveIndexPos[i] + 1) + '"]');
+        $('div[data-asin="' + dataAsin[i] + '"]').insertBefore('div[data-asin="' + firstDataAsinVal + '"]');
+    }
+    */
+    /*
+    for (var i = 0; i < items.length; i++) {
+        if ()
+    }
+    */
 });
 /*
     TO-DO
